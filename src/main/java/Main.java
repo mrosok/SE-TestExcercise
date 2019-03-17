6d8931134df2a24fbec21d6195eb69935f4951a9
Martin Røsok <martin.rosok@gmail.com>
2019-03-17 17:18:06 +0100

    added a Main method to print 1-100

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) throws TooNegativeException {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(HIOFNumbers.convert(i));
+        }
+    }
+}
